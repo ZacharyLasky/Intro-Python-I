bff42359f51dc2309c7068f4ddb6058125e426a1
Zach Lasky <zacharylasky123@gmail.com>
2019-12-03 18:53:56 -0500

    13 done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,10 +9,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+a = open("src/foo.txt", "r")
+print(a)
+a.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+b = open("src/bar.txt", "w")
+b.write('test1')
+b.write('test2')
+b.write('test3')
+b.close()
+b.open()
+print(b)
